b9fc56de1e15ae3a2e56fe27bc062f86944ef198
pedroaveber <pedro.veber@outlook.com>
2024-07-24 14:02:53 -0300

    feat: create casl ability and initialize server app

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteSomeoneElse = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteSomeoneElse)
+console.log(userCannotDeleteOtherUsers)
