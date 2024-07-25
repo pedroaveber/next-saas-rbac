46c9b163cee61d8a0cd665a0ee4d3ece5bafb985
pedroaveber <pedro.veber@outlook.com>
2024-07-24 21:17:07 -0300

    feat: create authentication middleware

---
+++ apps/api/src/http/middlewares/authentication-middleware.ts
@@ -0,0 +1,19 @@
+import type { FastifyInstance } from 'fastify'
+import { fastifyPlugin } from 'fastify-plugin'
+
+import { UnauthorizedError } from '../routes/_error/unauthorized-error'
+
+export const authenticationMiddleware = fastifyPlugin(
+  async (app: FastifyInstance) => {
+    app.addHook('preHandler', async (request) => {
+      request.getCurrentUserId = async () => {
+        try {
+          const { sub } = await request.jwtVerify<{ sub: string }>()
+          return sub
+        } catch {
+          throw new UnauthorizedError('Invalid authentication token.')
+        }
+      }
+    })
+  },
+)
--- apps/api/src/http/routes/authentication/get-authenticated-user-profile.ts
+++ apps/api/src/http/routes/authentication/get-authenticated-user-profile.ts
@@ -2,48 +2,51 @@ import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
+import { authenticationMiddleware } from '@/http/middlewares/authentication-middleware'
 import { prisma } from '@/lib/prisma'
 
 import { BadRequestError } from '../_error/bad-request-error'
 
 export async function getAuthenticatedUserProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        tags: ['Authentication'],
-        summary: 'Get authenticated user profile details',
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string().uuid(),
-              name: z.string().nullable(),
-              email: z.string().email(),
-              avatarUrl: z.string().url().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(authenticationMiddleware)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['Authentication'],
+          summary: 'Get authenticated user profile details',
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                email: z.string().email(),
+                avatarUrl: z.string().url().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>()
-      console.log(sub)
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
 
-      const user = await prisma.user.findUnique({
-        where: { id: sub },
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUrl: true,
-        },
-      })
+        const user = await prisma.user.findUnique({
+          where: { id: userId },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+        })
 
-      if (!user) {
-        throw new BadRequestError('User not found')
-      }
+        if (!user) {
+          throw new BadRequestError('User not found')
+        }
 
-      return reply.status(200).send({ user })
-    },
-  )
+        return reply.status(200).send({ user })
+      },
+    )
 }
